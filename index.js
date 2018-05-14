bbf2e510b31540040485fa22a788aaaf82dbbc4c
Thomas Kempel <thomask123@web.de>
2018-05-14 18:11:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
  * takes a predicate and a list and returns a pair of lists: 
  * those elements of the argument list that do and do not satisfy the predicate, respectively.
  * (a -> Bool) -> [a] -> ([a],[a])
+ 
  */
 function partition(predicate) {
-    return arr => arr.reduce((acc, cur) => acc[predicate(cur) ? 0 : 1].push(cur), [[], []]);
+    return arr => arr.reduce((acc, cur) => {
+        acc[predicate(cur) ? 0 : 1].push(cur);
+        return acc
+    }, [[], []]);
 }
 
 function quicksort([x, ...xs]) {
